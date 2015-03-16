efb244a47a0377b61e4e01da228e3cf4d767653e
wilson-weng <swenghkust@gmail.com>
2015-03-16 23:21:41 +0800

    init mobile version

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bokator Khmer Angkor Club' });
 });
-
+router.get('/mobile', function(req, res) {
+    res.render('mobile/index', { title: 'Bokator Khmer Angkor Club' });
+});
 module.exports = router;
